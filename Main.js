3ba3368bd998f874e1179cbbb92d9c306fc80ea5
BradleyAcosta <thescrill12@gmail.com>
2022-08-21 01:43:38 -0400

    yeap

--- Main.js
+++ Main.js
@@ -1,12 +1,16 @@
 documement.addEventListener("DOMContentLoaded", () => {
-const loginForm = document.querySelector("#login");
-const createAccountForm = document.querySelector("#createAccount");
-const linkCreateAccount = document.querySelector("linkCreateAccount");
-
-
- loginForm.addEventListener("click", () => {
-
-
- });
-
-});
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+    
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form__hidden");
+        createAccountForm.classlist.add("form__hidden");
+     });
+    
+        document.querySelector("#linkLogin").addEventListener("click", e => {
+            e.preventDefault();
+            loginForm.classList.add("form__hidden");
+            createAccountForm.classlist.remove("form__hidden");
+     });
+    });
