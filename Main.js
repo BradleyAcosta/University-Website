6950f09cab31e098d2ad1576ccf76da172447ffc
BradleyAcosta <thescrill12@gmail.com>
2022-08-21 01:43:13 -0400

    CHanges

--- Main.js
+++ Main.js
@@ -1,16 +1,12 @@
 documement.addEventListener("DOMContentLoaded", () => {
 const loginForm = document.querySelector("#login");
 const createAccountForm = document.querySelector("#createAccount");
+const linkCreateAccount = document.querySelector("linkCreateAccount");
 
-document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-    e.preventDefault();
-    loginForm.classList.remove("form__hidden");
-    createAccountForm.classlist.add("form__hidden");
+
+ loginForm.addEventListener("click", () => {
+
+
  });
 
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form__hidden");
-        createAccountForm.classlist.remove("form__hidden");
- });
 });
