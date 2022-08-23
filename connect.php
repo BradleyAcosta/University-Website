3dbe275d3ef1a1644235e04f0cc03f6d59fd2fb6
BradleyAcosta <thescrill12@gmail.com>
2022-08-23 17:46:53 -0400

    CAMBIE TODO PARA PHP

--- connect.php
+++ connect.php
@@ -6,5 +6,18 @@ $password = $_POST['password'];
 $confirmPassword = $_POST['passwordConfirm'];
 
 // database connection
+$conn = mysqli('localhost', 'root','', 'student_user' );
+if($conn->connect_error) {
 
+    die( 'Error connecting to: '.$conn->connect_error);
+} else {
+    $stmt = $conn->prepare("insert into registration(email, username, phonenumber, password,passwordConfirm)
+    value(?,?,?,?,?)");
+    $stmt->bind_param("sssssi", $email, $username, $phonenumber, $password, $passwordConfirm);
+    $stmt->execute();
+
+    echo "registration Succesfully...";
+    $stmt->close();
+    $con->close();
+}
 ?> 
