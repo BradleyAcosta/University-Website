cbedcce28475faa16fd8f489c5e92968084ed62a
BradleyAcosta <thescrill12@gmail.com>
2022-08-22 23:18:37 -0400

    PHP ADDED FOR SQL

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$email = $_POST['email'];
+$username = $_POST['username'];
+$phonenumber = $_POST['phonenumber'];
+$password = $_POST['password'];
+$confirmPassword = $_POST['passwordConfirm'];
+
+// database connection
+
+?> 
