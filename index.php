968e83a5169bdf9e66bea70f36a2942287f932d3
BradleyAcosta <thescrill12@gmail.com>
2022-08-23 18:24:00 -0400

    SEGUIMOS

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" type="text/css" href="style.css">
 
