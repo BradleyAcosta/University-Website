a4e3010bab047ddd6a13296ee904bfe6a9cdceeb
BradleyAcosta <thescrill12@gmail.com>
2022-08-24 12:43:31 -0400

    changes

--- signup.php
+++ signup.php
@@ -34,38 +34,11 @@ session_start();
 <html>
 <head>
 	<title>Signup</title>
+	
 </head>
 <body>
 
-	<style type="text/css">
-	
-	#text{
 
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
-
-	#button{
-
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-
-	</style>
 
 	<div id="box">
 		
