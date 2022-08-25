a49f459d05a571496f8fcb014d418d84ab5047c4
BradleyAcosta <thescrill12@gmail.com>
2022-08-25 14:20:52 -0400

    changes

--- register.php
+++ register.php
@@ -72,16 +72,16 @@ if (isset($_POST['reg_user'])) {
 		<form action="" method="POST" class="login-email">
             <p class="login-text" style="font-size: 2rem; font-weight: 800;">Register</p>
 			<div class="input-group">
-				<input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+				<input type="text" placeholder="Username" name="username" >
 			</div>
 			<div class="input-group">
-				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+				<input type="email" placeholder="Email" name="email">
 			</div>
 			<div class="input-group">
-				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+				<input type="password" placeholder="Password" name="password">
             </div>
             <div class="input-group">
-				<input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
+				<input type="password" placeholder="Confirm Password" name="cpassword">
 			</div>
 			<div class="input-group">
 				<button name="submit" class="btn">Register</button>
