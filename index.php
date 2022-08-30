f23dc87599d579ff3874f4810acf9c1be0155b6e
BradleyAcosta <thescrill12@gmail.com>
2022-08-29 23:50:11 -0400

    Changes

--- index.php
+++ index.php
@@ -1,66 +1,50 @@
-<?php 
-
-include 'config.php';
-
-session_start();
-
-error_reporting(0);
-
-if (isset($_SESSION['username'])) {
-    header("Location: welcome.php");
-}
-
-if (isset($_POST['submit'])) {
-	$email = $_POST['email'];
-	$password = md5($_POST['password']);
-
-	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
-	$result = mysqli_query($conn, $sql);
-	if ($result->num_rows > 0) {
-		$row = mysqli_fetch_assoc($result);
-		$_SESSION['username'] = $row['username'];
-		header("Location: welcome.php");
-	} else {
-		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
-	}
-}
-
-?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
 	<link rel="stylesheet" type="text/css" href="style.css">
 
-	<title>Login Form - </title>
+	<title>Login Form </title>
 </head>
 <body>
-	<div class="container">
-		<form action="" method="POST" class="login-email">
-			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
-			<div class="input-group">
-				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
-			</div>
-			<div class="input-group">
-				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
-			</div>
-			<div class="input-group">
-				<button name="submit" class="btn">Login</button>
-			</div>
-			<p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
+	<div class = "container">
+<div class = "header">
 
-			<div class="form-group">
+<h2> Login </h2>
 
+</div>
+
+<form action="index.php" method="post">
+<div>
+
+	<label for="username"> Username </label>
+	<input type="text" placeholder="Username" name="username" required>
+
+</div>
+
+<div class="input-group">
+
+	<label for="email"> Email </label>
+	<input type="email" placeholder="Email" name="email" required>
+
+</div>
+
+<div class="input-group">
+
+	 <label for="password"> password </label>
+	<input type="password" placeholder="Password" name="password" required>
+
+</div>
+
+<div class="input-group">
+<button type = "submit" name="reg_user" class="btn">Login</button>
+</div>
 			
+<p> Not a user? <a href="register.php"><b>Register here</b></a></p>
 
-		<a href="#"> Forgot Password?</a>
-
-		</div>
+	
 		</form>
 	</div>
 </body>
--- register.php
+++ register.php
@@ -1,48 +1,3 @@
-<?php 
-
-include 'config.php';
-
-error_reporting(0);
-
-session_start();
-
-if (isset($_SESSION['username'])) {
-    header("Location: index.php");
-}
-
-if (isset($_POST['submit'])) {
-	$username = $_POST['username'];
-	$email = $_POST['email'];
-	$password = ($_POST['password']);
-	$cpassword = ($_POST['cpassword']);
-
-	if ($password == $cpassword) {
-		$sql = "SELECT * FROM users WHERE email='$email'";
-		$result = mysqli_query($conn, $sql);
-		if (!$result->num_rows > 0) {
-			$sql = "INSERT INTO users (username, email, password)
-					VALUES ('$username', '$email', '$password')";
-			$result = mysqli_query($conn, $sql);
-			if ($result) {
-				echo "<script>alert('Wow! User Registration Completed.')</script>";
-				$username = "";
-				$email = "";
-				$_POST['password'] = "";
-				$_POST['cpassword'] = "";
-			} else {
-				echo "<script>alert('Woops! Something Wrong Went.')</script>";
-			}
-		} else {
-			echo "<script>alert('Woops! Email Already Exists.')</script>";
-		}
-		
-	} else {
-		echo "<script>alert('Password Not Matched.')</script>";
-	}
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -57,26 +12,44 @@ if (isset($_POST['submit'])) {
 </head>
 <body>
 	<div class="container">
-		<form action="" method="POST" class="login-email">
-            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Register</p>
-			<div class="input-group">
-				<input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+		<div class = "header">
+
+			<h2> Register </h2>
+
+		</div>
+
+		<form action="register.php" method="post">
+			<div>
+
+				<label for="username"> Username </label>
+				<input type="text" placeholder="Username" name="username" required>
+
 			</div>
+
 			<div class="input-group">
-				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+
+			    <label for="email"> Email </label>
+				<input type="email" placeholder="Email" name="email" required>
+
 			</div>
+
 			<div class="input-group">
-				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+
+		     	<label for="password"> password </label>
+				<input type="password" placeholder="Password" name="password" required>
+
             </div>
             <div class="input-group">
-				<input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
+
+			    <label for="password"> Confirm Password </label>
+				<input type="password" placeholder="Confirm Password" name="cpassword" required>
 			</div>
 			<div class="input-group">
-				<button name="submit" class="btn">Register</button>
+				<button type="submit" name="login_user" class="btn">Register</button>
 			</div>
-			<p class="login-register-text">Have an account? <a href="index.php">Login Here</a>.</p>
 
-		
+			<p>Have an account? <a href="index.php"><b>Login in</b></a></p>
+
 
 		</form>
 	</div>
