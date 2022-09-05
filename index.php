22ad8b84e13ac52d8fef7805dbd45238ffb20000
BradleyAcosta <thescrill12@gmail.com>
2022-09-05 16:46:06 -0400

    CSS CHANGES

--- index.php
+++ index.php
@@ -13,13 +13,26 @@ else{
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
     <title>Index</title>
   </head>
   <body>
-    <header>Welcome <?php echo $row["name"]; ?></header>
 
+   <nav>
+  <p>  Welcome <?php echo $row["name"]; ?></p>
+    <ul>
+    <li><a href ="index.php">Home</a></li>
+    <li><a href ="#">Contact</a>
+    <ul>
+      <li><a href = "#">Chat</a></li>
+    </ul>
+  </li>
+    <li><a href ="#">About Us</a></li>
+   
 
-
+    
+    </ul>
+   </nav>
 
 
 
