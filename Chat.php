4dab357ca22911bdb585b619b5f52472b5b136a0
Bradley Acosta Rojas <thescrill12@gmail.com>
2022-09-22 13:54:13 -0400

    More changes

--- Chat.php
+++ Chat.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Chat.css">
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="index.css">
 
     <title>Chat</title>
 </head>
@@ -13,12 +13,12 @@
 <nav>
   <p>  Welcome to chat </p>
     <ul>
-    <li><a href ="admin.php">Home</a></li>
+    <li><a href ="index.php">Home</a></li>
     <li><a href ="#">Curriculum</a>
 
     <li><a href ="#">Communication</a>
     <ul>
-      <li><a href = "AdminOrProfessor Chat.php">Chat</a></li>
+      <li><a href = "Chat.php">Chat</a></li>
       <li><a href = "#">Calendar</a></li>
     </ul>
   </li>
