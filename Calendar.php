f9351481127f7294ff6c0021409aca01a1922ae2
Bradley Acosta Rojas <thescrill12@gmail.com>
2022-10-13 14:33:27 -0400

    Better Calendar

--- Calendar.php
+++ Calendar.php
@@ -1,11 +1,3 @@
-<?php
-require 'CalendarConnection.php';
-
-$calendar = new Calendar();
-$calendar->setYear(2022);
-$calendar->setMonth(10);
-$calendar->create();
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,8 +7,19 @@ $calendar->create();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
     <title>Calendar/Date</title>
+	<!-- Bootstrap core CSS -->
+	
+		<!-- Custom styles for this template -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
 <link rel="stylesheet" href="index.css">
-<link rel="stylesheet" href="Chat.css">
+<link rel="stylesheet" href="Calendar.css">
+
 </head>
 <body>
 
@@ -40,38 +43,108 @@ $calendar->create();
     </ul>
    </nav>
 
+   <?php
+include('server.php');
+$query = $conn->query("SELECT * FROM events ORDER BY id");
+?>
+  <script>
+    $(document).ready(function() {
+     var calendar = $('#calendar').fullCalendar({
+      editable:true,
+      header:{
+       left:'prev,next today',
+       center:'title',
+       right:'month,agendaWeek,agendaDay'
+      },
+      events: [<?php while ($row = $query ->fetch_object()) { ?>{ id : '<?php echo $row->id; ?>', title : '<?php echo $row->title; ?>', start : '<?php echo $row->start_event; ?>', end : '<?php echo $row->end_event; ?>', }, <?php } ?>],
+      selectable:true,
+      selectHelper:true,
+      select: function(start, end, allDay)
+      {
+      var title = prompt("Enter Event Title");
+      if(title)
+      {
+        var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+        var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+        $.ajax({
+        url:"insert.php",
+        type:"POST",
+        data:{title:title, start:start, end:end},
+        success:function(data)
+        {
+          calendar.fullCalendar('refetchEvents');
+          alert("Added Successfully");
+          window.location.replace("Calendar.php");
+        }
+        })
+      }
+      },
+ 
+      editable:true,
+      eventResize:function(event)
+      {
+      var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+      var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+      var title = event.title;
+      var id = event.id;
+      $.ajax({
+        url:"update.php",
+        type:"POST",
+        data:{title:title, start:start, end:end, id:id},
+        success:function(){
+        calendar.fullCalendar('refetchEvents');
+        alert('Event Update');
+        }
+      })
+      },
+ 
+      eventDrop:function(event)
+      {
+      var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+      var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+      var title = event.title;
+      var id = event.id;
+      $.ajax({
+        url:"update.php",
+        type:"POST",
+        data:{title:title, start:start, end:end, id:id},
+        success:function()
+        {
+        calendar.fullCalendar('refetchEvents');
+        alert("Event Updated");
+        }
+      });
+      },
+ 
+      eventClick:function(event)
+      {
+      if(confirm("Are you sure you want to remove it?"))
+      {
+        var id = event.id;
+        $.ajax({
+        url:"delete.php",
+        type:"POST",
+        data:{id:id},
+        success:function()
+        {
+          calendar.fullCalendar('refetchEvents');
+          alert("Event Removed");
+        }
+        })
+      }
+      },
+ 
+    });
+  });
+</script>
+  <br />
+  <br />
+  <div class="container">
+  <h1> Calendar</h1>
+  <br>
+  <br>
+    <div id="calendar"></div>
 
-   <div id = "calendar" class = "container">
-  <table class="table table-bordered mt-4">
-	<thead>
-	<tr>
-		<?php 
-		foreach($calendar->getWeekDays() as $dayName): ?>
-		<th>
-
-		<?php echo $dayName; ?>
-
-		</th>
-		<?php endforeach; ?>
-	</tr>
-	</thead>
-		<tbody>
-			<?php foreach($calendar->getweeks() as $weeks): ?>
-				<tr>
-
-				<?php foreach($weeks as $dayNumber): ?>
-					<td>
-						<?php echo $dayNumber;?>
-					</td>
-					<?php endforeach; ?>
-				</tr>
-				<?php endforeach; ?>
-		</tbody>
-
-
-  </table>
-
-  </div>
-
-</body>
+   </div>
+ </body>
 </html>
