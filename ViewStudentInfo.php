095c2e38f087a92ad0701f54af8fc43d43fab78a
Bradley Acosta Rojas <thescrill12@gmail.com>
2022-10-28 13:05:38 -0400

    Added Table

--- ViewStudentInfo.php
+++ ViewStudentInfo.php
@@ -11,6 +11,9 @@ include('authentication.php');
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="CSS/index.css">
+    <link rel="stylesheet" href="CSS/Table.css">
+
+    <link rel="stylesheet" href="CSS/Admin.css">
     <link rel="stylesheet" href="CSS/Chat.css">
     <title>User dashboard</title>
   </head>
@@ -19,16 +22,16 @@ include('authentication.php');
    <nav>
     <ul>
     <li><a href ="index.php">Home</a></li>
-    <li><a href ="#">Curriculum</a>
+    <li><a href ="enrollment.php">Enrollment</a>
 
-    <li><a href ="enrollment.php">Communication</a>
+    <li><a href ="#">Communication</a>
     <ul>
       <li><a href = "StudentChat.php">Chat</a></li>
       <li><a href = "#">Calendar</a></li>
     </ul>
   </li>
   
-    <li><a href ="StudentAD.php">Student</a></li>
+    <li><a href ="ViewStudentInfo.php">Student</a></li>
 
     <li>
     <div class = "field button">
@@ -37,14 +40,88 @@ include('authentication.php');
    </div>
     </form>
     </li>
-    
-    </ul>
+ </ul>
    </nav>
 
    <div class="container">
   <h1> Students Information</h1>
 
+  
+  <table id="myDataTable" class="Table-class">
+  <thead>
+  <tr>
+ <th>ID</th>
+  <th>First Name</th>
+  <th>Username</th>
+  <th>Email</th>
+  <th>User Type</th>
+  <th>Edit</th>
+  <th>Delete</th>
+ </tr>
+</thead>
+<tbody>
+ <?php // Get Student information from database
+ $query = "SELECT * FROM registration";
+ $query_run = mysqli_query($conn, $query);
 
+ if(mysqli_num_rows($query_run) > 0)
+  {
+  foreach($query_run as $row)
+  {
+  ?>
+  <tr>
+  <td><?= $row['id']; ?></td>
+  <td><?= $row['name']; ?></td>
+   <td><?= $row['username']; ?></td>
+   <td><?= $row['email']; ?></td>
+     <td>
+   <?php
+  if($row['Check'] == '1'){
+  echo 'Admin';
+ }elseif($row['Check'] == '0'){
+  echo 'User';
+ }
+?>
+</td>
+                                    
+<td>
+<a href="register-edit.php?id=<?=$row['id'];?>" class="btn btn-success">Edit</a>
+</td>
+   <td>
+  <?php if($row['Check'] == '1'): ?>
+ <button type="button" class="btn btn-danger">No</button>
+ <?php else: ?>
+ <form action="Admin.php" method="POST">
+ <button type="submit" name="user_delete" value="<?=$row['id'];?>" class="btn btn-danger">Delete</button>
+  </form>
+        <?php endif; ?>
+   </td>
+  </tr>
+  <?php
+      }
+     }
+   else
+   {
+   ?>
+  <tr>
+  <td colspan="6">No Record Found</td>
+  </tr>
+   <?php
+   }
+  ?>
+                            
+ </tbody>
+ </table>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+  </body>
+</html>
 
    </div>
 
--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include('authentication.php');
    <nav>
     <ul>
     <li><a href ="index.php">Home</a></li>
-    <li><a href ="enrollment.php">Curriculum</a>
+    <li><a href ="enrollment.php">Enrollment</a>
 
     <li><a href ="#">Communication</a>
     <ul>
