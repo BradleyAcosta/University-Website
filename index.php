68519ba36337190a54772b2be37f6026b2d8c051
Bradley Acosta Rojas <thescrill12@gmail.com>
2022-10-28 14:31:37 -0400

    Working on Enrollment

--- index.php
+++ index.php
@@ -100,60 +100,7 @@ include('authentication.php');
             </div>
           </div>
         <br>
-        <?php if (isset($_POST['showinfo'])) {
-          $choose= $_POST['choose'];
-          $roll = $_POST['roll'];
-          if (!empty($choose && $roll)) {
-            $query = mysqli_query($db_con,"SELECT * FROM `student_info` WHERE `roll`='$roll' AND `class`='$choose'");
-            if (!empty($row=mysqli_fetch_array($query))) {
-              if ($row['roll']==$roll && $choose==$row['class']) {
-                $stroll= $row['roll'];
-                $stname= $row['name'];
-                $stclass= $row['class'];
-                $city= $row['city'];
-                $photo= $row['photo'];
-                $pcontact= $row['pcontact'];
-              ?>
-        <div class="row">
-          <div class="col-sm-6 offset-sm-3">
-            <table class="table table-bordered">
-              <tr>
-                <td rowspan="5"><h3>Student Info</h3><img class="img-thumbnail" src="admin/images/<?= isset($photo)?$photo:'';?>" width="250px"></td>
-                <td>Name</td>
-                <td><?= isset($stname)?$stname:'';?></td>
-              </tr>
-              <tr>
-                <td>Roll</td>
-                <td><?= isset($stroll)?$stroll:'';?></td>
-              </tr>
-              <tr>
-                <td>Class</td>
-                <td><?= isset($stclass)?$stclass:'';?></td>
-              </tr>
-              <tr>
-                <td>City</td>
-                <td><?= isset($city)?$city:'';?></td>
-              </tr>
-              <tr>
-                <td>Submit Date</td>
-                <td><?= isset($pcontact)?$pcontact:'';?></td>
-              </tr>
-            </table>
-          </div>
-        </div>  
-      <?php 
-          }else{
-                echo '<p style="color:red;">Please Input Valid Roll & Email</p>';
-              }
-            }else{
-              echo '<p style="color:red;">Your Input Doesn\'t Match!</p>';
-            }
-            }else{?>
-              <script type="text/javascript">alert("Data Not Found!");</script>
-            <?php }
-          }; ?>
-    </div>
-
+      
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
