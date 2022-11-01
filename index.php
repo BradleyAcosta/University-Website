c1d6bd28079191994172b0074dcb50bf0a8f53eb
Bradley Acosta Rojas <thescrill12@gmail.com>
2022-10-31 20:41:34 -0400

    Changes

--- index.php
+++ index.php
@@ -45,66 +45,6 @@ include('authentication.php');
    <div class="container">
    <p><?php include('message.php'); //Display any error on succeed Message?></p>
 
-          <div class="row">
-            <div class="col-md-4 offset-md-4">
-              <form method="POST">
-            <table class="text-center infotable">
-              <tr>
-                <th colspan="2">
-                  <p class="text-center">Student Information</p>
-                </th>
-              </tr>
-              <tr>
-                <td>
-                   <p>Choose Class</p>
-                </td>
-                <td>
-                   <select class="form-control" name="choose">
-                     <option value="">
-                       Select
-                     </option>
-                     <option value="1st">
-                       1St
-                     </option>
-                     <option value="2nd">
-                       2nd
-                     </option>
-                     <option value="3rd">
-                       3rd
-                     </option>
-                     <option value="4th">
-                       4th
-                     </option>
-                     <option value="5th">
-                       5th
-                     </option>
-                   </select>
-                </td>
-              </tr>
-
-              <tr>
-                <td>
-                  <p><label for="roll">Roll No</label></p>
-                </td>
-                <td>
-                  <input class="form-control" type="text" pattern="[0-9]{6}" id="roll" placeholder="Roll Num.." name="roll">
-                </td>
-              </tr>
-              <tr>
-                <td colspan="2" class="text-center">
-                  <input class="btn btn-danger" type="submit" name="showinfo">
-                </td>
-              </tr>
-            </table>
-          </form>
-            </div>
-          </div>
-        <br>
-      
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+         
   </body>
 </html>
