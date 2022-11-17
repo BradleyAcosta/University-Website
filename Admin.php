bbc93e94430bd0445999abbbca174e125e98a674
Bradley Acosta Rojas <thescrill12@gmail.com>
2022-11-17 14:45:27 -0500

    WEBSITE DONE

--- Admin.php
+++ Admin.php
@@ -11,6 +11,7 @@ include('authentication.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="CSS/Admin.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/Table.css">
 
     <title>Admin dashboard</title>
