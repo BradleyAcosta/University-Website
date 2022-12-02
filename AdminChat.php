4009d3bf6d895f5850fcf66fbe3c8d49d2d06073
Bradley Acosta Rojas <thescrill12@gmail.com>
2022-12-01 21:23:09 -0500

    Working with Chat

--- AdminChat.php
+++ AdminChat.php
@@ -1,3 +1,9 @@
+<?php
+require 'database/server.php';
+include('allcode.php');
+include('authentication.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +11,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="src/stylesheet" href="CSS/Chat.css">
+    <link rel="src/stylesheet" href="CSS/Table.css">
+
     <link rel="stylesheet" href="CSS/Admin.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
@@ -41,8 +49,25 @@
     <header> 
       
     <h1>Hello   </h1>
-     <h3>Welcome to chat! Were you can communicate with us.</h3>
-    
+     <h3>Welcome to chat! </h3>
+<br>
+<br>
+
+    <p>Send Message to:</p>
+    <ul>
+  <?php
+
+  $msgs = mysqli_query($conn, "SELECT * FROM registration") or die("Failed to query database");
+  while($msg = mysqli_fetch_assoc($msgs)) {
+    echo '<li><a href ="?toUser='.$msg["id"].'">'.$msg["name"].'</a></li>';
+  }
+
+
+
+
+  ?>
+
+    </ul>
 
 
      
